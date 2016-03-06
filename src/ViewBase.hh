265d4898b6c6e31b108484209ee85e8c5af5412d
Michael F. Herbst <info@michael-herbst.com>
2016-03-06 19:23:37 +0100

    Refactor and use SubscriptionPointer
    for storing both lazy and non-lazy matrices

--- src/ViewBase.hh
+++ src/ViewBase.hh
@@ -8,20 +8,44 @@
 namespace linalgwrap {
 namespace view {
 
-/** \brief View base interface satisfied by both ViewBase classes*/
+/** \brief Class which contains and manages the reference
+ *         to the Matrix we view upon.
+ */
 template <typename Matrix>
-class ViewBase_i {
+class BaseViewMatrixContainer {
     typedef Matrix inner_matrix_type;
 
-  public:
-    virtual void update(const ParameterMap&) = 0;
+    /** A swap function for ViewMatrixContainer */
+    friend void swap(BaseViewMatrixContainer& first,
+                     BaseViewMatrixContainer& second) {
+        using std::swap;  // enable ADL
+
+        swap(first.m_inner_ptr, second.m_inner_ptr);
+    }
+
+    /** \brief Construct a container from a matrix reference and an identifier.
+     *
+     * The identifier is used in order to make the subscription with the matrix
+     * and as an identification for the actual type of view.
+     */
+    BaseViewMatrixContainer(inner_matrix_type& inner,
+                            const std::string& identifier)
+          : m_inner_ptr{make_subscription(inner, identifier)} {}
 
   protected:
     /** Access the inner matrix as a reference */
-    virtual inner_matrix_type& inner_matrix() = 0;
+    inner_matrix_type& inner_matrix() override { return *m_inner_ptr; }
 
     /** Access the inner matrix as a const reference */
-    virtual const inner_matrix_type& inner_matrix() const = 0;
+    const inner_matrix_type& inner_matrix() const override {
+        return *m_inner_ptr;
+    }
+
+    std::string indentifier() const { return m_inner_ptr.subscriber_id(); }
+
+  private:
+    //! Store a copy of the inner lazy matrix expression:
+    SubscriptionPointer<inner_matrix_type> m_inner_ptr;
 };
 
 /** \brief View base class for Views of lazy matrices
@@ -36,9 +60,11 @@ template <typename Matrix,
 class ViewBase
       : public LazyMatrixExpression<
               typename std::remove_const<Matrix>::type::stored_matrix_type>,
-        public ViewBase_i<Matrix> {
+        public BaseViewMatrixContainer<Matrix> {
   public:
-    typedef Matrix inner_matrix_type;
+    typedef BaseViewMatrixContainer<Matrix> container_type;
+    typedef typename container_type::inner_matrix_type inner_matrix_type;
+
     typedef LazyMatrixExpression<typename std::remove_const<
           inner_matrix_type>::type::stored_matrix_type> base_type;
     typedef typename base_type::stored_matrix_type stored_matrix_type;
@@ -56,8 +82,8 @@ class ViewBase
     friend void swap(ViewBase& first, ViewBase& second) {
         using std::swap;  // enable ADL
 
-        swap(first.m_inner, second.m_inner);
-        swap(first.m_identifier, second.m_identifier);
+        swap(static_cast<container_type&>(first),
+             static_cast<container_type&>(second));
         swap(static_cast<base_type&>(first), static_cast<base_type&>(second));
     }
 
@@ -70,7 +96,7 @@ class ViewBase
      * \param identifier An identifier for the type of view.
      */
     ViewBase(inner_matrix_type& inner, const std::string& identifier)
-          : m_inner{inner}, m_identifier(identifier) {}
+          : container_type(inner, identifier){};
 
     ViewBase(const ViewBase&) = default;
     ViewBase(ViewBase&&) = default;
@@ -82,28 +108,16 @@ class ViewBase
      *         given the ParameterMap
      * */
     void update(const ParameterMap& map) override {
-        CallUpdateIfAvail<Matrix> mat;
-        mat(m_inner, map);
-        // m_inner.update(map)
+        CallUpdateIfAvail<Matrix> call_update;
+        call_update(container_type::inner_matrix(), map);
     }
 
-    /** \brief Print the expression tree to this outstream */
+    /** \brief Print the expression tree to this outstream
+     * */
     void print_tree(std::ostream& o) const override {
-        o << m_identifier << " of ";
-        inner_matrix().print_tree(o);
+        o << container_type::indentifier() << " of ";
+        container_type::inner_matrix().print_tree(o);
     }
-
-  protected:
-    /** Access the inner matrix as a const reference */
-    const inner_matrix_type& inner_matrix() const override { return m_inner; }
-
-    /** Access the inner matrix as a reference */
-    inner_matrix_type& inner_matrix() override { return m_inner; }
-
-  private:
-    //! Store a copy of the inner lazy matrix expression:
-    inner_matrix_type m_inner;
-    std::string m_identifier;
 };
 
 /** \brief View base class for Views of stored matrices
@@ -115,9 +129,11 @@ class ViewBase
 template <typename Matrix>
 class ViewBase<Matrix, std::true_type>
       : public LazyMatrixExpression<typename std::remove_const<Matrix>::type>,
-        public ViewBase_i<Matrix> {
+        public BaseViewMatrixContainer<Matrix> {
   public:
-    typedef Matrix inner_matrix_type;
+    typedef BaseViewMatrixContainer<Matrix> container_type;
+    typedef typename container_type::inner_matrix_type inner_matrix_type;
+
     typedef LazyMatrixExpression<typename std::remove_const<Matrix>::type>
           base_type;
     typedef typename base_type::stored_matrix_type stored_matrix_type;
@@ -130,7 +146,8 @@ class ViewBase<Matrix, std::true_type>
     friend void swap(ViewBase& first, ViewBase& second) {
         using std::swap;  // enable ADL
 
-        swap(first.m_inner_ptr, second.m_inner_ptr);
+        swap(static_cast<container_type&>(first),
+             static_cast<container_type&>(second));
         swap(static_cast<base_type&>(first), static_cast<base_type&>(second));
     }
 
@@ -144,7 +161,7 @@ class ViewBase<Matrix, std::true_type>
      *                   constructor.
      */
     ViewBase(inner_matrix_type& inner, const std::string& identifier)
-          : m_inner_ptr{make_subscription(inner, identifier)} {}
+          : container_type(inner, identifier){};
 
     ViewBase(const ViewBase&) = default;
     ViewBase(ViewBase&&) = default;
@@ -163,20 +180,9 @@ class ViewBase<Matrix, std::true_type>
     /** \brief Print the expression tree to this outstream
      * */
     void print_tree(std::ostream& o) const override {
-        o << m_inner_ptr.subscriber_id() << " of " << inner_matrix().name();
+        o << container_type::identifier() << " of "
+          << container_type::inner_matrix().name();
     }
-
-  protected:
-    /** Access the inner matrix as a reference */
-    inner_matrix_type& inner_matrix() override { return *m_inner_ptr; }
-
-    /** Access the inner matrix as a const reference */
-    const inner_matrix_type& inner_matrix() const override {
-        return *m_inner_ptr;
-    }
-
-  private:
-    SubscriptionPointer<inner_matrix_type> m_inner_ptr;
 };
 
 }  // view
