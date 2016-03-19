d904288864400692a54adf473833b425218a4544
Michael F. Herbst <info@michael-herbst.com>
2016-03-19 18:19:25 +0100

    Restructure version file

--- src/version.hh
+++ src/version.hh
@@ -1,5 +1,15 @@
 #pragma once
+#include <string>
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 #define VERSION_REVISION 0
+
+struct version {
+    static int constexpr major{VERSION_MAJOR};
+    static int constexpr minor{VERSION_MINOR};
+    static int constexpr revision{VERSION_REVISION};
+
+    // Return the version as a string
+    static std::string version_string();
+};
