0ff5872f2644e316c19aa29c39d77497049ced04
Michael F. Herbst <info@michael-herbst.com>
2016-03-22 08:16:00 +0100

    Add Parameter to specify minimal length

--- tests/generators/RangeWithin.hh
+++ tests/generators/RangeWithin.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include <rapidcheck.h>
 #include <Range.hh>
+#include <sstream>
 
 namespace rc {
 
@@ -9,15 +10,28 @@ struct RangeWithin {
     typedef T value_type;
 
     //! Construct a range which at most runs over the interval [min:max)
-    static Gen<linalgwrap::Range<value_type>> range_within(value_type min,
-                                                           value_type max) {
+    static Gen<linalgwrap::Range<value_type>> range_within(
+          value_type min, value_type max, value_type min_length = 0) {
 
         auto gen_range = [=] {
+            if (min > max) {
+                std::stringstream ss;
+                ss << "Minimum " << min << " not smaller or equal to maximum "
+                   << max;
+                throw rc::GenerationFailure(ss.str());
+            }
+
+            if (min_length >= max - min) {
+                std::stringstream ss;
+                ss << "Minimal length " << min_length
+                   << " must be smaller than the difference between max and min"
+                   << max - min;
+                throw rc::GenerationFailure(ss.str());
+            }
+
             if (min == max) return linalgwrap::Range<value_type>{min, max};
 
-            RC_PRE(min < max);
-
-            auto length = *gen::inRange<value_type>(0, max - min);
+            auto length = *gen::inRange<value_type>(min_length, max - min);
             auto start = *gen::inRange<value_type>(min, max - length);
 
             return linalgwrap::Range<value_type>{start, start + length};
@@ -32,8 +46,8 @@ struct RangeWithin {
  */
 namespace gen {
 template <typename T>
-Gen<linalgwrap::Range<T>> range_within(T min, T max) {
-    return RangeWithin<T>::range_within(min, max);
+Gen<linalgwrap::Range<T>> range_within(T min, T max, T min_length = 0) {
+    return RangeWithin<T>::range_within(min, max, min_length);
 }
 }  // namespace gen
 }  // namespace rc
