9a97ebe91a6d9f82a03a4c9767cf5dae9e46f209
Michael F. Herbst <info@michael-herbst.com>
2016-04-08 15:55:14 +0200

    Add more perfect forwarding

--- src/tuple_utils.hh
+++ src/tuple_utils.hh
@@ -10,102 +10,86 @@
 
 namespace linalgwrap {
 namespace detail {
-//! Implementation to get a tuple of references to the tail of a tuple.
+//! Implementation to get the tuple tail
 template <std::size_t... Indices, typename E, typename... Es>
-std::tuple<Es&...> tuple_tail_impl(std::index_sequence<Indices...>,
-                                   std::tuple<E&, Es&...> t);
+constexpr std::tuple<Es...> tuple_tail_impl(std::index_sequence<Indices...>,
+                                            std::tuple<E, Es...>&& t);
 
 //! Implementation of the ref function for tuple references
 //  We want to return a tuple of references from a referenced tuple.
 template <std::size_t... Indices, typename... Es>
-std::tuple<Es&...> tuple_ref_impl(std::index_sequence<Indices...>,
-                                  std::tuple<Es...>& t);
+constexpr std::tuple<Es&...> tuple_ref_impl(std::index_sequence<Indices...>,
+                                            std::tuple<Es...>& t);
 }  // namespace detail
 
 /** \name Basic tuple operations */
 ///@{
-/** Get the head of a std::tuple of references
- *
- * \note You can use tuple_ref to make a tuple of references out of a
- *       referenced tuple
- * */
+/** Get the head of a std::tuple. */
 template <typename E, typename... Es>
-E& tuple_head(std::tuple<E&, Es&...> t);
+constexpr E& tuple_head(std::tuple<E, Es...>& t);
 
-/** Get the tail of a std::tuple of references
- *
- * \note You can use tuple_ref to make a tuple of references out of a
- *       referenced tuple
- * */
+/** Get the head of a const std::tuple. */
 template <typename E, typename... Es>
-std::tuple<Es&...> tuple_tail(std::tuple<E&, Es&...> t);
+constexpr const E& tuple_head(const std::tuple<E, Es...>& t);
+
+/** Get the tail of a std::tuple, taking ownership of the original object. */
+template <typename E, typename... Es>
+constexpr std::tuple<Es...> tuple_tail(std::tuple<E, Es...>&& t);
 
 /** Construct a tuple taking ownership of head and tail. */
 template <typename E, typename... Es>
-std::tuple<E, Es...> tuple_cons(E&& e, std::tuple<Es...>&& t);
+constexpr std::tuple<E, Es...> tuple_cons(E&& e, std::tuple<Es...>&& t);
 
 /** Construct a tuple of references to each element of the referenced tuple */
 template <typename... Es>
-std::tuple<Es&...> tuple_ref(std::tuple<Es...>& t);
+constexpr std::tuple<Es&...> tuple_ref(std::tuple<Es...>& t);
 ///@}
 
 /** \name Tuple traversal and mapping */
 ///@{
 //@{
 /** \brief Apply the operation op to the first element
- *         in a tuple of references, which matches a predicate.
+ *         in a tuple, which matches a predicate.
  *
  *         Does not apply the operation if no object
  *         matches the predicate
- *
- * \note You can use tuple_ref to make a tuple of references out of a
- *       referenced tuple
  */
 template <typename Pred, typename Op, typename... Ts>
-void tuple_for_first(Pred pred, Op op, std::tuple<Ts&...> t);
+constexpr void tuple_for_first(Pred&& pred, Op&& op, std::tuple<Ts...>&& t);
 
 // End recursion
 template <typename Pred, typename Op>
-void tuple_for_first(Pred, Op, std::tuple<>);
+constexpr void tuple_for_first(Pred&&, Op&&, std::tuple<>);
 //@}
 
 //@{
-/** \brief Apply a unary operator to all elements of a tuple of references
- *         in turn.
- *
- * \note You can use tuple_ref to make a tuple of references out of a
- *       referenced tuple
- **/
+/** \brief Apply a unary operator to all elements of a tuple in turn. */
 template <typename UnOp, typename... Ts>
-void tuple_for_each(UnOp op, std::tuple<Ts&...> t);
+constexpr void tuple_for_each(UnOp&& op, std::tuple<Ts...>&& t);
 
 // Special case to end recursion
 template <typename UnOp>
-void tuple_for_each(UnOp, std::tuple<>);
+constexpr void tuple_for_each(UnOp&&, std::tuple<>);
 //@}
 
 //@{
-/** \brief Map a unary operator to all elements of a tuples of references
- *         in turn.
+/** \brief Map a unary operator to all elements of a tuple  in turn.
  *
  * Return the tuple of all results.
  *
- *  Of cause the apply function of the operator should be generic in
+ *  Of cause the operator() of the UnOp should be generic in
  *  all types occurring.
  *
  * \note The operator has to return a non-void object, otherwise this
  * implementation fails. Use tuple_for_each for unary operators
  * returning void.
- *
- * \note You can use tuple_ref to make a tuple of references out of a
- *       referenced tuple
  *  */
 template <typename UnOp, typename... Ts>
-auto tuple_map(UnOp op, std::tuple<Ts&...> t);
+constexpr auto tuple_map(UnOp&& op, std::tuple<Ts...>&& t);
 
 // Special case to end recursion.
 template <typename UnOp>
-std::tuple<> tuple_map(UnOp, std::tuple<>);
+constexpr std::tuple<> tuple_map(UnOp&&, std::tuple<>);
 //@}
 
 //@{
@@ -120,12 +104,13 @@ std::tuple<> tuple_map(UnOp, std::tuple<>);
  * implementation fails. Use for_each for operators
  * returning void.
  *  */
+template <typename BinOp, typename... Tlhs, typename... Trhs>
+constexpr auto tuple_map(BinOp&& op, std::tuple<Tlhs...>&& lhs,
+                         std::tuple<Trhs...>&& rhs);
+
 // Special case to end recursion.
 template <typename BinOp>
-std::tuple<> tuple_map(BinOp, std::tuple<>, std::tuple<>);
-
-template <typename BinOp, typename... Tlhs, typename... Trhs>
-auto tuple_map(BinOp op, std::tuple<Tlhs&...> lhs, std::tuple<Trhs&...> rhs);
+constexpr std::tuple<> tuple_map(BinOp&&, std::tuple<>, std::tuple<>);
 //@}
 ///@}
 
@@ -135,78 +120,105 @@ auto tuple_map(BinOp op, std::tuple<Tlhs&...> lhs, std::tuple<Trhs&...> rhs);
 
 namespace detail {
 template <std::size_t... Indices, typename E, typename... Es>
-inline std::tuple<Es&...> tuple_tail_impl(std::index_sequence<Indices...>,
-                                          std::tuple<E&, Es&...> t) {
-    // Return a tuple of the references we extract with get.
-    return std::tie(std::get<Indices + 1u>(t)...);
+constexpr std::tuple<Es...> tuple_tail_impl(std::index_sequence<Indices...>,
+                                            std::tuple<E, Es...>&& t) {
+    typedef std::tuple<E, Es...> tuple_t;
+    typedef std::tuple<Es...> tail_t;
+
+    // Return a tuple with the first element removed
+    return tail_t{std::get<Indices + 1u>(std::forward<tuple_t>(t))...};
 }
 
 template <std::size_t... Indices, typename... Es>
-inline std::tuple<Es&...> tuple_ref_impl(std::index_sequence<Indices...>,
-                                         std::tuple<Es...>& t) {
+constexpr std::tuple<Es&...> tuple_ref_impl(std::index_sequence<Indices...>,
+                                            std::tuple<Es...>& t) {
     // Return a tuple of the references we extract with get.
     return std::tie(std::get<Indices>(t)...);
 }
 }  // namespace detail
 
 template <typename E, typename... Es>
-inline E& tuple_head(std::tuple<E&, Es&...> t) {
+constexpr E& tuple_head(std::tuple<E, Es...>& t) {
+    return std::get<0>(t);
+}
+
+template <typename E, typename... Es>
+constexpr const E& tuple_head(const std::tuple<E, Es...>& t) {
     return std::get<0>(t);
 }
 
 /** Get the tail of a std::tuple reference*/
 template <typename E, typename... Es>
-inline std::tuple<Es&...> tuple_tail(std::tuple<E&, Es&...> t) {
+constexpr std::tuple<Es...> tuple_tail(std::tuple<E, Es...>&& t) {
+    typedef std::tuple<E, Es...> tuple_t;
+
     // First construct an index sequence from 0 to the number of types in Es
     auto idcs = std::make_index_sequence<sizeof...(Es)>();
 
     // Then call the implementation function
-    return detail::tuple_tail_impl(idcs, t);
+    return detail::tuple_tail_impl(idcs, std::forward<tuple_t>(t));
 }
 
 template <typename E, typename... Es>
-inline std::tuple<E, Es...> tuple_cons(E&& e, std::tuple<Es...>&& t) {
+constexpr std::tuple<E, Es...> tuple_cons(E&& e, std::tuple<Es...>&& t) {
     return std::tuple_cat(std::tuple<E>{e}, t);
 }
 
 template <typename... Es>
-std::tuple<Es&...> tuple_ref(std::tuple<Es...>& t) {
+constexpr std::tuple<Es&...> tuple_ref(std::tuple<Es...>& t) {
     auto idcs = std::make_index_sequence<sizeof...(Es)>();
     return detail::tuple_ref_impl(idcs, t);
 }
 
 template <typename Pred, typename Op, typename... Ts>
-inline void tuple_for_first(Pred pred, Op op, std::tuple<Ts&...> t) {
+constexpr void tuple_for_first(Pred&& pred, Op&& op, std::tuple<Ts...>&& t) {
+    typedef std::tuple<Ts...> tuple_t;
+
     if (pred(tuple_head(t))) {
         op(tuple_head(t));
         return;
     } else {
-        tuple_for_first(std::forward<Pred>(pred), std::forward<Op>(op), t);
+        // Extract tail:
+        auto tail = tuple_tail(std::forward<tuple_t>(t));
+
+        // Recurse into tail:
+        tuple_for_first(std::forward<Pred>(pred), std::forward<Op>(op),
+                        std::move(tail));
     }
 }
 
 template <typename Pred, typename Op>
-inline void tuple_for_first(Pred, Op, std::tuple<>) {}
+constexpr void tuple_for_first(Pred&&, Op&&, std::tuple<>) {}
 
 template <typename UnOp, typename... Ts>
-inline void tuple_for_each(UnOp op, std::tuple<Ts&...> t) {
+constexpr void tuple_for_each(UnOp&& op, std::tuple<Ts...>&& t) {
+    typedef std::tuple<Ts...> tuple_t;
+
     // Apply to head:
     op(tuple_head(t));
 
+    // Extract tail:
+    auto tail = tuple_tail(std::forward<tuple_t>(t));
+
     // Do operation on tail:
-    return tuple_for_each(std::forward<UnOp>(op), tuple_tail(t));
+    return tuple_for_each(std::forward<UnOp>(op), std::move(tail));
 }
 
 template <typename UnOp>
-inline void tuple_for_each(UnOp, std::tuple<>) {}
+constexpr void tuple_for_each(UnOp&&, std::tuple<>) {}
 
 template <typename UnOp, typename... Ts>
-inline auto tuple_map(UnOp op, std::tuple<Ts&...> t) {
+constexpr auto tuple_map(UnOp&& op, std::tuple<Ts...>&& t) {
+    typedef std::tuple<Ts...> tuple_t;
+
     // Apply op to head:
     auto res_head = op(head(t));
 
+    // Extract tail:
+    auto tail = tuple_tail(std::forward<tuple_t>(t));
+
     // Do operation (recursively) on the tail:
-    auto res_tail = tuple_map(std::forward<UnOp>(op), tail(t));
+    auto res_tail = tuple_map(std::forward<UnOp>(op), std::move(tail));
 
     // Cons the results together:
     return tuple_cons(std::move(res_head), std::move(res_tail));
@@ -214,18 +226,13 @@ inline auto tuple_map(UnOp op, std::tuple<Ts&...> t) {
 
 // Special case to end recursion.
 template <typename UnOp>
-inline std::tuple<> tuple_map(UnOp, std::tuple<>) {
-    return std::tuple<>{};
-}
-
-template <typename BinOp>
-inline std::tuple<> tuple_map(BinOp, std::tuple<>, std::tuple<>) {
+constexpr std::tuple<> tuple_map(UnOp&&, std::tuple<>) {
     return std::tuple<>{};
 }
 
 template <typename BinOp, typename... Tlhs, typename... Trhs>
-inline auto tuple_map(BinOp op, std::tuple<Tlhs&...> lhs,
-                      std::tuple<Trhs&...> rhs) {
+constexpr auto tuple_map(BinOp&& op, std::tuple<Tlhs...>&& lhs,
+                         std::tuple<Trhs...>&& rhs) {
     static_assert(sizeof...(Tlhs) == sizeof...(Trhs),
                   "Both tuples to map for the case of binary operations "
                   "need to be of the same size.");
@@ -238,14 +245,20 @@ inline auto tuple_map(BinOp op, std::tuple<Tlhs&...> lhs,
     auto res_head = op(lhs_head, rhs_head);
 
     // Extract tail (as tuple of references)
-    auto lhs_tail = tuple_tail(lhs);
-    auto rhs_tail = tuple_tail(rhs);
+    auto lhs_tail = tuple_tail(std::forward<std::tuple<Tlhs...>>(lhs));
+    auto rhs_tail = tuple_tail(std::forward<std::tuple<Trhs...>>(rhs));
 
     // Do operation (recursively) on the tail:
-    auto res_tail = tuple_map(std::forward<BinOp>(op), lhs_tail, rhs_tail);
+    auto res_tail = tuple_map(std::forward<BinOp>(op), std::move(lhs_tail),
+                              std::move(rhs_tail));
 
     // Cons the results together:
     return tuple_cons(std::move(res_head), std::move(res_tail));
+}
+
+template <typename BinOp>
+constexpr std::tuple<> tuple_map(BinOp&&, std::tuple<>, std::tuple<>) {
+    return std::tuple<>{};
 }
 
 }  // namespace linalgwrap
