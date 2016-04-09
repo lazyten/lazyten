4edd3bba9bde68cf1e3a976b495464184ce72426
Michael F. Herbst <info@michael-herbst.com>
2016-04-09 11:52:59 +0200

    Add copyright notice inside files

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,4 +1,31 @@
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2016 by the linalgwrap authors
+##
+## This file is part of linalgwrap.
+##
+## linalgwrap is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## linalgwrap is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with linalgwrap. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+
 import os
+
+# This file is loosely based upon the file
+# cpp/ycm/.ycm_extra_conf.py from the youcompleteme daemon process
+# available on github:
+# https://github.com/Valloric/ycmd/blob/master/cpp/ycm/.ycm_extra_conf.py
+
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
