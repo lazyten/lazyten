3d874f2fd4fe3b7b63681b17888a30a09c5bdb59
Michael F. Herbst <info@michael-herbst.com>
2016-06-24 23:18:15 +0200

    Whitespace changes
    Instead of 4 spaces use 2 spaces of indention.

--- src/linalgwrap/Constants.hh
+++ src/linalgwrap/Constants.hh
@@ -25,29 +25,29 @@ namespace linalgwrap {
 
 template <typename Scalar>
 struct Constants {
-  typedef Scalar scalar_type;
+    typedef Scalar scalar_type;
 
-  //! Constant representing 0
-  static constexpr scalar_type zero = scalar_type(0.);
+    //! Constant representing 0
+    static constexpr scalar_type zero = scalar_type(0.);
 
-  //! Constant representing 1
-  static constexpr scalar_type one = scalar_type(1.);
+    //! Constant representing 1
+    static constexpr scalar_type one = scalar_type(1.);
 
-  /** \brief Constant which should be used for invalid values
-   *
-   * \note This value may well be a valid ``scalar_type`` value, so
-   * do not check against it in order to see if the ``scalar_type`` value
-   * is invalid.
-   */
-  static constexpr scalar_type invalid =
-        std::numeric_limits<scalar_type>::has_signaling_NaN
-              ? std::numeric_limits<scalar_type>::signaling_NaN()
-              : std::numeric_limits<scalar_type>::max();
+    /** \brief Constant which should be used for invalid values
+     *
+     * \note This value may well be a valid ``scalar_type`` value, so
+     * do not check against it in order to see if the ``scalar_type`` value
+     * is invalid.
+     */
+    static constexpr scalar_type invalid =
+          std::numeric_limits<scalar_type>::has_signaling_NaN
+                ? std::numeric_limits<scalar_type>::signaling_NaN()
+                : std::numeric_limits<scalar_type>::max();
 
-  /** \brief Constant which gives the default numerical tolerance to use
-   *  for a number of checks */
-  static constexpr scalar_type default_tolerance =
-        std::numeric_limits<scalar_type>::epsilon();
+    /** \brief Constant which gives the default numerical tolerance to use
+     *  for a number of checks */
+    static constexpr scalar_type default_tolerance =
+          std::numeric_limits<scalar_type>::epsilon();
 };
 
 template <typename Scalar>
