6eccd410ad11158e2dcdeeaa3ce19a12ab4989ec
Michael F. Herbst <info@michael-herbst.com>
2016-09-02 10:53:33 +0200

    Script to update cmake modules and scripts from krims automatically
    Some scripts and module of the build system need to be shipped with every
    repository. The update_from_sister_repos.sh script allows to update
    these files automatically from one master repository.

---
+++ update_from_sister_repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. update_from_sisters.lib.sh || exit 1
+
+update_file "krims" "external/get.lib.sh" || exit 1
+update_file "krims" "external/get_rapidcheck.sh" || exit 1
+update_file "krims" "cmake/findRapidcheck.cmake" || exit 1
+update_file "krims" "cmake/findCatch.cmake" || exit 1
