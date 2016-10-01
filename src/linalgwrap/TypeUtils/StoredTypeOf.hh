56b596d9ab685d5e3b617cf75f0b3b36ea7e38b9
Michael F. Herbst <info@michael-herbst.com>
2016-10-01 12:56:50 +0200

    Add StoredTypeOf type util struct
    Determines the underlying stored type of a Matrix. If this matrix is a
    stored matrix, no changes to the type are done.

---
+++ src/linalgwrap/TypeUtils/StoredTypeOf.hh
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2016 by the linalgwrap authors
+//
+// This file is part of linalgwrap.
+//
+// linalgwrap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// linalgwrap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with linalgwrap. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+#include "linalgwrap/LazyMatrixExpression.hh"
+#include <krims/TypeUtils.hh>
+
+namespace linalgwrap {
+
+//@{
+/** \brief helper struct to extract the underlying stored matrix
+ *         type from a potentially lazy matrix.
+ *
+ * The stored matrix type is accessible via the member type "type".
+ *  */
+template <typename Matrix, typename = void>
+struct StoredTypeOf {
+    // neither a lazy matrix nor a stored matrix
+    // => no clue what to do.
+    typedef void type;
+};
+
+template <typename Matrix>
+struct StoredTypeOf<
+      Matrix, typename std::enable_if<IsStoredMatrix<Matrix>::value>::type> {
+    typedef Matrix type;
+};
+
+template <typename Matrix>
+struct StoredTypeOf<
+      Matrix, typename std::enable_if<IsLazyMatrix<Matrix>::value>::type> {
+    typedef typename Matrix::stored_matrix_type type;
+};
+//@}
+
+}  // namespace linalgwrap
