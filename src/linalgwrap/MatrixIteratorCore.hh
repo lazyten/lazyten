0083e6294afaebe556e3a24710d4ab9303194d23
Michael F. Herbst <info@michael-herbst.com>
2016-11-14 08:13:40 +0100

    Move EnforceReference inside MatrixIteratorCore

--- src/linalgwrap/MatrixIteratorCore.hh
+++ src/linalgwrap/MatrixIteratorCore.hh
@@ -19,7 +19,6 @@
 
 #pragma once
 #include "linalgwrap/Constants.hh"
-#include "linalgwrap/EnforceReference.hh"
 #include "linalgwrap/Exceptions.hh"
 #include "linalgwrap/Matrix_i.hh"
 #include "linalgwrap/StoredMatrix_i.hh"
@@ -37,6 +36,49 @@ template <typename Scalar>
 class StoredMatrix_i;
 
 namespace detail {
+
+/** \brief Class to enforce a reference to be returned
+ *
+ * The class requires prior knowledge about the situation
+ * Only in the case that we get the data by-value it really
+ * does anything (it copies the data to internal storage
+ * and returns a reference to it)
+ *
+ * \tparam T The data type
+ * \tparam FromValue Do we get the data by-value(true) or by-reference(false)
+ * */
+template <typename T, bool FromValue>
+struct EnforceReference {};
+
+/** \brief Class to enforce a reference to be returned
+ *
+ * This version is an identity operation, just returning
+ * the reference it got.
+ */
+template <typename T>
+struct EnforceReference<T, false> {
+    typedef T& result_type;
+    typedef T& argument_type;
+    T& operator()(T& t) const { return t; }
+};
+
+/** \brief Class to enforce a reference to be returned
+ *
+ * This version copies the value to internal storage and
+ * returns a reference to this internal value.
+*/
+template <typename T>
+struct EnforceReference<T, true> {
+    typedef T& result_type;
+    typedef T argument_type;
+    T& operator()(T t) const {
+        dummy = t;
+        return dummy;
+    }
+
+  private:
+    mutable T dummy;
+};
 
 /** \brief Base class providing the required inner functionality of an iterator
  *
