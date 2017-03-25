3cfbbbf321a92995ca9f633b5396ada655afa87b
Michael F. Herbst <info@michael-herbst.com>
2017-03-25 20:50:37 +0100

    Add clang tools targets
    This commit adds the targets clang-format-linalgwrap and
    clang-tidy-linalgwrap, which run the respective clang tools over the
    source tree.

--- update_from_sister_repos.sh
+++ update_from_sister_repos.sh
@@ -23,6 +23,7 @@
 . update_from_sisters.lib.sh || exit 1
 
 update_file "krims" ".clang-format" || exit 1
+update_file "krims" ".clang-tidy" || exit 1
 update_file "krims" "external/get.lib.sh" || exit 1
 update_file "krims" "external/get_rapidcheck.sh" || exit 1
 update_file "krims" "cmake/findRapidcheck.cmake" || exit 1
