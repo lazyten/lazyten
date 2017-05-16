9a8fa1503778a9ce3ef51e0966edec123bf4a47e
Michael F. Herbst <info@michael-herbst.com>
2017-05-16 15:58:28 +0200

    Fix typos and make example code more readable

--- examples/eigenproblem_demo/main.cc
+++ examples/eigenproblem_demo/main.cc
@@ -17,12 +17,10 @@
 // along with linalgwrap. If not, see <http://www.gnu.org/licenses/>.
 //
 
+#include "matrices.hh"
 #include <linalgwrap/DiagonalMatrix.hh>
-#include <linalgwrap/SmallMatrix.hh>
 #include <linalgwrap/SmallVector.hh>
 #include <linalgwrap/eigensystem.hh>
-
-using namespace linalgwrap;
 
 template <typename Solution>
 void print_solution(const Solution& solution) {
@@ -33,24 +31,9 @@ void print_solution(const Solution& solution) {
 }
 
 int main() {
-  typedef double scalar_type;
-  typedef SmallMatrix<scalar_type> matrix_type;
+  using namespace linalgwrap;
 
-  // A hermitian test matrix
-  matrix_type mat_a{{76.52963291652406, -25.666510871673864, 34.43577632850656,
-                     -45.36273708264996, 22.754426259474258, -11.270092801526317},
-                    {-25.666510871673864, -82.22395447280388, 51.60398579599948,
-                     20.19904424373368, 46.73986164186124, -19.391628960288173},
-                    {34.43577632850656, 51.60398579599948, -44.14761987677002,
-                     37.103640372970546, -23.036847741099365, 22.45800147301634},
-                    {-45.36273708264996, 20.19904424373368, 37.103640372970546,
-                     52.40569142155442, 7.539858840849945, 28.047790646596525},
-                    {22.754426259474258, 46.73986164186124, -23.036847741099365,
-                     7.539858840849945, -30.003084289104322, -80.44779473550247},
-                    {-11.270092801526317, -19.391628960288173, 22.45800147301634,
-                     28.047790646596525, -80.44779473550247, 64.66122950159928}};
-
-  // Compute the 4 largest real eigenpairs
+  // Compute the 4 largest real eigenpairs of the test matrix a
   const size_t n_ep = 4;
   krims::GenMap map{{"which", "LR"}};
 
@@ -64,12 +47,12 @@ int main() {
   std::cout << std::endl;
 
   //
-  // Compute solution to the eigensystem using Armadillo
+  // Compute solution to the eigensystem using Lapack
   //
-  map.update("method", "armadillo");
+  map.update("method", "lapack");
   const auto solution_armadillo = linalgwrap::eigensystem_hermitian(mat_a, n_ep, map);
 
-  std::cout << "Arpack eigenpairs: " << std::endl;
+  std::cout << "Lapack eigenpairs: " << std::endl;
   print_solution(solution_armadillo);
   std::cout << std::endl;
 
@@ -84,6 +67,7 @@ int main() {
 
     const auto sum = diagmatrix + mat_a;
     const auto solution_lazy = linalgwrap::eigensystem_hermitian(sum, n_ep, map);
+
     std::cout << "Lazy matrix eigenpairs: " << std::endl;
     print_solution(solution_lazy);
     std::cout << std::endl;
@@ -94,39 +78,16 @@ int main() {
   // given by two lazy matrix expressions
   //
   {
-    map.update("method", "auto");
-    SmallMatrix<scalar_type> mat_a{
-          {-37.827087025623456, 7.737383999517007, -6.868688554610827, 0.4582402410539501,
-           20.25022267287818, -7.264272891184383, 17.13528963873486, -0.6958437952983445},
-          {7.737383999517007, -8.907240678731085, -10.491465613577162,
-           -32.400293305571346, 1.9050289475850448, 38.08086976760322, 33.58637188467661,
-           16.93991633946149},
-          {-6.868688554610827, -10.491465613577162, -40.64686080355078,
-           26.642223861646016, 26.870722616736714, 10.117350139698132,
-           -0.19698340817648585, 14.714415823222282},
-          {0.4582402410539501, -32.400293305571346, 26.642223861646016,
-           -28.78493388224115, -10.56060566681876, -7.7461795092692824,
-           -9.341941942209004, -3.682243002650196},
-          {20.25022267287818, 1.9050289475850448, 26.870722616736714, -10.56060566681876,
-           -35.21142298086606, 4.5353204853178966, 41.307563515495005,
-           -10.243149796300195},
-          {-7.264272891184383, 38.08086976760322, 10.117350139698132, -7.7461795092692824,
-           4.5353204853178966, 46.221528657079915, -11.029394211181724,
-           33.71103101586654},
-          {17.13528963873486, 33.58637188467661, -0.19698340817648585, -9.341941942209004,
-           41.307563515495005, -11.029394211181724, 24.566705894503286,
-           -5.017034388900839},
-          {-0.6958437952983445, 16.93991633946149, 14.714415823222282, -3.682243002650196,
-           -10.243149796300195, 33.71103101586654, -5.017034388900839,
-           13.035884650583398}};
-
     SmallVector<scalar_type> diagonal{1, 2, 3, 1, 2, 4, 8, 9};
     auto diagmatrix = make_diagmat(std::move(diagonal));
+    const auto sum = mat_b + 100 * diagmatrix;
 
+    // Compute 2 largest eigenpairs
     const size_t n_ep = 2;
-    const auto sum = mat_a + 100 * diagmatrix;
+    const krims::GenMap params{{"method", "auto"}, {"which", "LR"}};
     const auto solution_lgen =
-          linalgwrap::eigensystem_hermitian(sum, diagmatrix, n_ep, map);
+          linalgwrap::eigensystem_hermitian(sum, diagmatrix, n_ep, params);
+
     std::cout << "Lazy matrix generalised eigenpairs: " << std::endl;
     print_solution(solution_lgen);
     std::cout << std::endl;
