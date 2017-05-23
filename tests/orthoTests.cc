5d7abb7046ff1e137770bb72b60e04875fe34c9b
Michael F. Herbst <info@michael-herbst.com>
2017-05-23 11:04:39 +0200

    Add basic orthogonormalisation routine
    The routine is a modified Gram-Schmidt, which is both slow and not
    extremely numerically stable.

---
+++ tests/orthoTests.cc
@@ -0,0 +1,102 @@
+//
+// Copyright (C) 2017 by the linalgwrap authors
+//
+// This file is part of linalgwrap.
+//
+// linalgwrap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// linalgwrap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with linalgwrap. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <catch.hpp>
+#include <linalgwrap/SmallVector.hh>
+#include <linalgwrap/TestingUtils.hh>
+#include <linalgwrap/ortho.hh>
+#include <linalgwrap/random.hh>
+
+namespace linalgwrap {
+namespace tests {
+using namespace rc;
+
+TEST_CASE("Test ortho function", "[ortho]") {
+  typedef double scalar_type;
+  typedef SmallVector<double> vector_type;
+  typedef SmallMatrix<double> matrix_type;
+
+  auto gen_id = []() {
+    const size_t n_dim = *gen::map(gen::numeric_size<2>(), [](size_t i) {
+                            return i + 3;
+                          }).as("Dimensionality");
+
+    // The identity matrix:
+    matrix_type id(n_dim, n_dim);
+    for (size_t i = 0; i < n_dim; ++i) id(i, i) = 1;
+
+    return id;
+  };
+
+  auto gen_pos_dev_mat = [&gen_id]() {
+    // TODO Use the WithProperties generator once it has
+    //      support for arbitrary positive definite matrices.
+    auto id = gen_id();
+    for (size_t i = 0; i < id.n_cols(); ++i) {
+      id(i, i) *= *gen::map(gen::numeric_around(1.5), [](scalar_type in) {
+                     return std::abs(in);
+                   }).as("Weighting factor w");
+    }
+    return id;
+  };
+
+  auto testable_pre = [](matrix_type m, bool is_id) {
+    const size_t n_vecs = *gen::inRange<size_t>(2, m.n_cols()).as("Number of vectors");
+
+    auto gen_non_zero_vec =
+          gen::construct<vector_type>(gen::container<std::vector<scalar_type>>(
+                m.n_cols(),
+                gen::mapcat(gen::exec([] { return random<scalar_type>(); }),
+                            [](scalar_type s) { return gen::numeric_around(s); })));
+    const auto vecs =
+          *gen::numeric_tensor<MultiVector<vector_type>>(
+                 n_vecs, gen::with_l2_norm_in_range(0.2, 100, gen_non_zero_vec))
+                 .as("Vectors to orthogonalise.");
+    std::cout << vecs.n_vectors() << " " << vecs.n_elem() << std::endl;
+    std::cout << vecs << std::endl << std::endl;
+
+    // Orthogonalise
+    const auto res = is_id ? ortho(vecs) : ortho(vecs, m);
+
+    // Build scalar products
+    const matrix_type innprod = dot(res, m * res);
+
+    // The identity matrix:
+    matrix_type id(n_vecs, n_vecs);
+    for (size_t i = 0; i < n_vecs; ++i) id(i, i) = 1;
+
+    RC_ASSERT(numcomp(id) == innprod);
+  };
+
+  SECTION("Orthogonalise random real vectors") {
+    CHECK(rc::check("Orthogonalise random real vectors",
+                    [&testable_pre, &gen_id]() { testable_pre(gen_id(), true); }));
+  }
+
+  SECTION("M-orthogonalise random real vectors") {
+    CHECK(rc::check("M-orthogonalise random real vectors",
+                    [&testable_pre, &gen_pos_dev_mat]() {
+                      testable_pre(gen_pos_dev_mat(), false);
+                    }));
+  }
+
+}  // Test ortho function
+
+}  // namespace tests
+}  // namespace linalgwrap
